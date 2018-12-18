f6ab3051edc11cf23f3ec77337be79a65cdd11b9
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-12-18 21:40:22 +0200

    tag again

--- pages/_slug.vue
+++ pages/_slug.vue
@@ -6,7 +6,7 @@
       </h1>
     </div>
     <div class="col-1-2">
-      <template v-html="payload.content.rendered"/>
+      <div v-html="payload.content.rendered"/>
     </div>
   </div>
 </template>

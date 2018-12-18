eebc559476ff3d3c658c5fe6d122c4b779c5916d
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-12-18 22:29:59 +0200

    test remove base url

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,7 +21,6 @@ module.exports = {
   ** Environment variables
   */
   env: {
-    baseUrl: process.env.BASE_URL || 'http://localhost:3000',
     baseEndpoint: 'http://yamen.vimlyhost.com/wp/wp-json/',
     baseVersion: 'wp/v2'
   },

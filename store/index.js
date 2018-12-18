55781302f4466058595d4c8991b7cf8c90d29cb7
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-12-18 10:48:17 +0200

    sets up environment vars + store

---
+++ store/index.js
@@ -0,0 +1,12 @@
+import Vuex from 'vuex'
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: () => ({}),
+    getters: {},
+    actions: {},
+    mutations: {}
+  })
+}
+
+export default createStore

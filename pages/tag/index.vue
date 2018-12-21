744f845cfc7c6cc4410f04c23bc2b0935d15dcbd
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-12-21 15:22:15 +0200

    nuxt-link

--- pages/tag/index.vue
+++ pages/tag/index.vue
@@ -6,7 +6,7 @@
     <li 
       v-for="(tag, slug) in tags" 
       :key="slug">
-      <a :href="`/tag/${slug}`"> {{ tag.name }} </a>
+      <nuxt-link :to="`/tag/${slug}`"> {{ tag.name }} </nuxt-link>
     </li>
   </div>
 </template>

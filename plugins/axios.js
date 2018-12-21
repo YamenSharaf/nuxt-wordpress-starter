f68d86d590d1538b9544d6da5fa8d81e678723ae
Yamen Sharaf <YamenSharaf@Gmail.com>
2018-12-21 20:04:46 +0200

    logs config

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,6 @@
 export default function(context) {
   context.$axios.onRequest(config => {
+    console.log('config', config)
     console.log('CONTEXT', context)
     console.log('Making request to ' + config.url)
   })
